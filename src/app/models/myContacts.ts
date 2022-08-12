e940da32c9a6acd4e372fd471f66e23df57d20ba
apurva52 <55528747+apurva52@users.noreply.github.com>
2022-08-12 11:45:36 +0530

    Create myContacts.ts

---
+++ src/app/models/myContacts.ts
@@ -0,0 +1,16 @@
+export class MyContact {
+    id?:string;
+    name?:string;
+    email?:string
+    photo?:string;
+    mobile?:string;
+    company?:string;
+    title?:string;
+    groupId?:string;
+    createdTime?:string | Date | number;
+}
+
+export const credentials = {
+    loginid:'admin',
+    password :'@dmin'
+}
